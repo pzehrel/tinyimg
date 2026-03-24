46143a82242919486b0bfbbf576857b53aa7e274
pzehrel <pzehrel@gmail.com>
2026-03-24 16:45:21 +0800

    Phase 03: Cache System
    Merge phase 03 cache system implementation

--- packages/tinyimg-core/src/index.ts
+++ packages/tinyimg-core/src/index.ts
@@ -1,15 +1,21 @@
+// Cache
+export { calculateMD5 } from './cache/hash'
+export { getGlobalCachePath, getProjectCachePath } from './cache/paths'
+
+export { type CacheStats, formatBytes, getAllCacheStats, getCacheStats } from './cache/stats'
+export { CacheStorage, readCache, writeCache } from './cache/storage'
 // Config loading
 export { loadKeys } from './config/loader'
 export type { LoadedKey } from './config/loader'
-
 // Errors
 export { AllKeysExhaustedError, NoValidKeysError } from './errors/types'
+
 // Key management
 export { maskKey } from './keys/masker'
+
 export { KeyPool, type KeyStrategy } from './keys/pool'
 export { createQuotaTracker, queryQuota } from './keys/quota'
 export { PrioritySelector, RandomSelector, RoundRobinSelector } from './keys/selector'
-
 export { validateKey } from './keys/validator'
 
 // Utils
